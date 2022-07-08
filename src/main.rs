aa970253c0a38ccb2ff9d53bcbb0f324978a015e
Sean Barnard <sean.barnard.94@hotmail.com>
2022-07-08 11:39:32 +0100

    Rewrite to copy config from vault.txt file

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,63 @@
-use cli_clipboard::{ClipboardContext, ClipboardProvider};
-use std::fs::OpenOptions;
-use std::io::prelude::*;
+use std::{fs::{File, OpenOptions}, io::prelude::*};
 use std::path::Path;
 
 #[derive(Debug)]
 struct Config {
     access_key: String,
-    secret_access_key: String,
-    session_token: String,
-}
-
-impl Config {
-    fn new(access_key: String, secret_access_key: String, session_token: String) -> Self {
-        Config {
-            access_key,
-            secret_access_key,
-            session_token,
-        }
-    }
+    secret_key: String,
+    security_token: String,
 }
 
 fn main() {
-    let new_config = get_config_from_clipboard();
-    write_new_credentials_file(new_config)
+    let config = read_config_from_vault_file();
+    write_new_credentials_file(config);
 }
 
-fn get_config_from_clipboard() -> Config {
-    let mut ctx = ClipboardContext::new().unwrap();
-    let contents = ctx.get_contents().unwrap();
-    let configs: Vec<&str> = contents.lines().collect();
-    if configs.len() < 4 {
-        panic!("Please copy your aws config to clipboard and try again");
-    }
+fn read_config_from_vault_file() -> Config {
+    let mut access_key = String::from("aws_access_key_id=");
+    let mut secret_key = String::from("aws_secret_access_key=");
+    let mut security_token = String::from("aws_session_token=");
 
-    let new_config = Config::new(
-        String::from(configs[1]),
-        String::from(configs[2]),
-        String::from(configs[3]),
-    );
-    println!(
-        "{}\n{}\n{}",
-        new_config.access_key, new_config.secret_access_key, new_config.session_token
-    );
-    new_config
+    let path = Path::new("/mnt/c/Users/sean.barnard/.aws/vault.txt");
+    match File::open(&path) {
+        Ok(mut file) => {
+            let mut contents = String::new();
+
+            match file.read_to_string(&mut contents) {
+                Ok(_) => {
+                    let split = contents.split("\n");
+                    for line in split {
+                        if line.starts_with("access_key") {
+                            let parts: Vec<&str> = line.split_whitespace().collect();
+                            access_key.push_str(&parts[1]);
+                            println!("{}", parts[1]);
+                        };
+                        if line.starts_with("secret_key") {
+                            let parts: Vec<&str> = line.split_whitespace().collect();
+                            secret_key.push_str(&parts[1]);
+                            println!("{}", parts[1]);
+                        };
+                        if line.starts_with("security_token") {
+                            let parts: Vec<&str> = line.split_whitespace().collect();
+                            security_token.push_str(&parts[1]);
+                            println!("{}", parts[1]);
+                        };
+                    }
+                }
+                Err(err) => {
+                    println!("{}", err);
+                }
+            }
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
+    Config {
+        access_key,
+        secret_key,
+        security_token,
+    }
 }
 
 fn write_new_credentials_file(cfg: Config) {
@@ -57,7 +72,7 @@ fn write_new_credentials_file(cfg: Config) {
     write
         .write_fmt(format_args!(
             "[default]\n{}\n{}\n{}",
-            cfg.access_key, cfg.secret_access_key, cfg.session_token
+            cfg.access_key, cfg.secret_key, cfg.security_token
         ))
         .expect("unable to write data");
 }
