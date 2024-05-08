ffe584f5d4730c11840b59378f13570f7fb41884
Sean Barnard <sean.barnard.94@hotmail.com>
2024-05-08 10:36:23 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use std::{fs::{File, OpenOptions}, io::prelude::*};
+#![allow(dead_code)]
+use std::io;
 use std::path::Path;
+use std::process::{Command, Stdio};
+use std::{
+    fs::{File, OpenOptions},
+    io::prelude::*,
+};
 
 #[derive(Debug)]
 struct Config {
@@ -9,8 +15,36 @@ struct Config {
 }
 
 fn main() {
-    let config = read_config_from_vault_file();
-    write_new_credentials_file(config);
+    get_vault_config();
+    // let config = read_config_from_vault_file();
+    // write_new_credentials_file(config);
+}
+
+fn get_vault_config() {
+    println!("'dev' or 'qa'");
+    let mut env_choice = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut env_choice).unwrap_or_default();
+
+    Command::new("/mnt/c/Hashicorp/vault.exe")
+        .arg("write")
+        .arg("account/865412956762/sts/Owner")
+        .arg("ttl=4h")
+        .spawn()
+        .expect("something went wrong with command 1")
+        .wait()
+        .expect("");
+
+    // .arg("|")
+    // .arg("Out-File")
+    // .arg("-Encoding")
+    // .arg("UTF8")
+    // .arg("-FilePath")
+    // .arg("/mnt/c/Users/barnars/.aws/vault.txt")
+    // .spawn()
+    // .expect("something went wrong running the binary")
+    // .wait()
+    // .expect("");
 }
 
 fn read_config_from_vault_file() -> Config {
@@ -18,7 +52,7 @@ fn read_config_from_vault_file() -> Config {
     let mut secret_key = String::from("aws_secret_access_key=");
     let mut security_token = String::from("aws_session_token=");
 
-    let path = Path::new("/mnt/c/Users/sean.barnard/.aws/vault.txt");
+    let path = Path::new("/mnt/c/Users/barnars/.aws/vault.txt");
     match File::open(&path) {
         Ok(mut file) => {
             let mut contents = String::new();
